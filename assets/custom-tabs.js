e9e0aedb197d1e424f50c6483017082743d0d077
mazal-tov-traders <d90.german@gmail.com>
2025-04-10 22:59:57 +0300

    fix styles for mobile

--- assets/custom-tabs.js
+++ assets/custom-tabs.js
@@ -1,43 +1,44 @@
 class CustomTabs extends HTMLElement {
     constructor() {
-        super();
+      super();
     }
-
+  
     connectedCallback() {
-        const tabButtons = this.querySelectorAll('[role="tab"]');
-        const tabContents = this.querySelectorAll('[role="tabcontent"]');
-
-        const handleTabClick = (event) => {
-            if (tabContents) {
-                tabContents.forEach((content) => {
-                    content.hidden = true;
-                });
-            }
-
-            if (tabButtons) {
-                tabButtons.forEach((tabButton) => {
-                    tabButton.setAttribute('aria-selected', false);
-                });
-            }
-
-            event.currentTarget.setAttribute('aria-selected', true);
-            const { id } = event.currentTarget;
-            const tabContent = this.querySelector(`[aria-labelledby="${id}"]`);
-
-            if (tabContent) {
-                tabContent.hidden = false;
-            }
-        };
-
-        if (tabButtons) {
-            tabButtons.forEach((button, index) => {
-                button.addEventListener('click', handleTabClick);
-                if (index === 0) {
-                    handleTabClick({ currentTarget: button });
-                }
-            });
+      const tabButtons = this.querySelectorAll('[role="tab"]');
+      const tabContents = this.querySelectorAll('[role="tabcontent"]');
+      const timerBlocks = document.querySelectorAll('.custom-timers-wrapper .custom-timer-block');
+  
+      const handleTabClick = (event) => {
+        const currentButton = event.currentTarget;
+        const tabId = currentButton.id;
+        const content = this.querySelector(`[aria-labelledby="${tabId}"]`);
+        const handle = content?.dataset.handle;
+  
+        tabContents.forEach((el) => el.hidden = true);
+        tabButtons.forEach((btn) => btn.setAttribute('aria-selected', false));
+  
+        currentButton.setAttribute('aria-selected', true);
+        if (content) content.hidden = false;
+  
+        timerBlocks.forEach((block) => {
+          block.hidden = true;
+        });
+  
+        if (handle) {
+          const timer = document.querySelector(`.custom-timer-block[data-handle="${handle}"]`);
+          if (timer) timer.hidden = false;
         }
+      };
+  
+      tabButtons.forEach((button, index) => {
+        button.addEventListener('click', handleTabClick);
+  
+        if (index === 0) {
+          handleTabClick({ currentTarget: button });
+        }
+      });
     }
-}
-
-customElements.define('custom-tabs', CustomTabs);
+  }
+  
+  customElements.define('custom-tabs', CustomTabs);
+  
